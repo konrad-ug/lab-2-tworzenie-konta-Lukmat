ceef10d1ee6c29c574a3523b16e45f37ab016c9f
Łukasz Mróz <mr.oz2001@gmail.com>
2022-11-10 17:41:08 +0100

    updated feature12

--- app/tests/test_loan.py
+++ app/tests/test_loan.py
@@ -7,54 +7,55 @@ class TestCreateBankAccount(unittest.TestCase):
     lastname = "Januszewski"
     pesel = "02310634908"
 
+    def setUp(self):
+        self.account = Konto(self.name, self.lastname, self.pesel)
+
     def test_successful_loan_1(self):
-        account = Konto(self.name, self.lastname, self.pesel)
-        account.history = [-100, 100, 100, 100]
-        is_granted = account.take_out_loan(500)
+        self.account.history = [-100, 100, 100, 100]
+        is_granted = self.account.take_out_loan(500)
         self.assertTrue(is_granted)
-        self.assertEqual(account.balance, 500)
+        self.assertEqual(self.account.balance, 500)
 
     def test_successful_loan_2(self):
-        account = Konto(self.name, self.lastname, self.pesel)
-        account.history = [400, -50, 200, 100, -100]
-        is_granted = account.take_out_loan(500)
+        self.account.history = [400, -50, 200, 100, -100]
+        is_granted = self.account.take_out_loan(500)
         self.assertTrue(is_granted)
-        self.assertEqual(account.balance, 500)
+        self.assertEqual(self.account.balance, 500)
 
     def test_unsuccessful_loan_1(self):
-        account = Konto(self.name, self.lastname, self.pesel)
-        account.history = [-100, -100, 200, -100, 200]
-        is_granted = account.take_out_loan(500)
+        self.account.history = [-100, -100, 200, -100, 200]
+        is_granted = self.account.take_out_loan(500)
         self.assertFalse(is_granted)
-        self.assertEqual(account.balance, 0)
+        self.assertEqual(self.account.balance, 0)
 
     def test_unsuccessful_loan_2(self):
-        account = Konto(self.name, self.lastname, self.pesel)
-        account.history = [-100, -100, 400, -100, 400]
-        is_granted = account.take_out_loan(500)
+        self.account.history = [-100, -100, 400, -100, 400]
+        is_granted = self.account.take_out_loan(500)
         self.assertFalse(is_granted)
-        self.assertEqual(account.balance, 0)
+        self.assertEqual(self.account.balance, 0)
 
     def test_unsuccessful_loan_3(self):
-        account = Konto(self.name, self.lastname, self.pesel)
-        account.history = [-100, -100, 400, -100, 100]
-        is_granted = account.take_out_loan(500)
+        self.account.history = [-100, -100, 400, -100, 100]
+        is_granted = self.account.take_out_loan(500)
         self.assertFalse(is_granted)
-        self.assertEqual(account.balance, 0)
+        self.assertEqual(self.account.balance, 0)
 
     def test_unsuccessful_loan_4(self):
-        account = Konto(self.name, self.lastname, self.pesel)
-        account.history = [100, 500]
-        is_granted = account.take_out_loan(500)
+        self.account.history = [100, 500]
+        is_granted = self.account.take_out_loan(500)
         self.assertFalse(is_granted)
-        self.assertEqual(account.balance, 0)
+        self.assertEqual(self.account.balance, 0)
 
     def test_unsuccessful_loan_5(self):
-        account = Konto(self.name, self.lastname, self.pesel)
-        account.history = []
-        is_granted = account.take_out_loan(500)
+        self.account.history = []
+        is_granted = self.account.take_out_loan(500)
         self.assertFalse(is_granted)
-        self.assertEqual(account.balance, 0)
-
+        self.assertEqual(self.account.balance, 0)
+    
+    def test_unsuccessful_loan_6(self):
+        self.account.history = [100, 200, 100, -200]
+        is_granted = self.account.take_out_loan(500)
+        self.assertFalse(is_granted)
+        self.assertEqual(self.account.balance, 0)
     
 
