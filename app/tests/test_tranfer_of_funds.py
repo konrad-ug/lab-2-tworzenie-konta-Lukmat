ba13e561a5774e6d155eee4613deee56372ea1a0
Łukasz Mróz <mr.oz2001@gmail.com>
2022-12-21 23:51:50 +0100

    added feature17

--- app/tests/test_tranfer_of_funds.py
+++ app/tests/test_tranfer_of_funds.py
@@ -2,6 +2,7 @@ import unittest
 
 from ..Konto import Konto
 from ..KontoFirmowe import KontoFirmowe
+from unittest.mock import patch
 
 class TestProcessTransfers(unittest.TestCase):
     name = "Dariusz"
@@ -9,7 +10,7 @@ class TestProcessTransfers(unittest.TestCase):
     pesel = "02310634908"
 
     company_name = "Januszex sp. z o.o"
-    nip = "6289735952"
+    nip = "5252674798"
 
     def test_successful_outgoing_tranfer(self):
         account = Konto(self.name, self.lastname, self.pesel)
@@ -38,25 +39,33 @@ class TestProcessTransfers(unittest.TestCase):
         account.process_incoming_transfer(200)
         self.assertEqual(account.balance, 1200)
 
-    def test_successful_outgoing_tranfer_for_company_accounts(self):
+    @patch('app.KontoFirmowe.KontoFirmowe.is_nip_in_gov')
+    def test_successful_outgoing_tranfer_for_company_accounts(self, mock_is_nip_in_gov):
+        mock_is_nip_in_gov.return_value = True
         account = KontoFirmowe(self.company_name, self.nip)
         account.balance = 1000
         account.process_outgoing_transfer(800)
         self.assertEqual(account.balance, 1000 - 800)
 
-    def test_unsuccessful_outgoing_transfer_for_company_accounts(self):
+    @patch('app.KontoFirmowe.KontoFirmowe.is_nip_in_gov')
+    def test_unsuccessful_outgoing_transfer_for_company_accounts(self, mock_is_nip_in_gov):
+        mock_is_nip_in_gov.return_value = True
         account = KontoFirmowe(self.company_name, self.nip)
         account.balance = 1000
         account.process_outgoing_transfer(1200)
         self.assertEqual(account.balance, 1000)
 
-    def test_successful_incoming_tranfer_for_company_accounts(self):
+    @patch('app.KontoFirmowe.KontoFirmowe.is_nip_in_gov')
+    def test_successful_incoming_tranfer_for_company_accounts(self, mock_is_nip_in_gov):
+        mock_is_nip_in_gov.return_value = True
         account = KontoFirmowe(self.company_name, self.nip)
         account.balance = 0
         account.process_incoming_transfer(800)
         self.assertEqual(account.balance, 0 + 800)
 
-    def test_series_of_transfers_for_company_accounts(self):
+    @patch('app.KontoFirmowe.KontoFirmowe.is_nip_in_gov')
+    def test_series_of_transfers_for_company_accounts(self, mock_is_nip_in_gov):
+        mock_is_nip_in_gov.return_value = True
         account = KontoFirmowe(self.company_name, self.nip)
         account.balance = 0
         account.process_incoming_transfer(2000)
